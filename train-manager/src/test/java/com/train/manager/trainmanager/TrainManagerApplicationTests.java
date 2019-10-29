04c4900e366c1fe24785dbb25dd315af7c1bfa1a
hjdlc99 <hjdlc99@163.com>
2019-10-29 13:54:08 +0800

    初始化框架

---
+++ train-manager/src/test/java/com/train/manager/trainmanager/TrainManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.train.manager.trainmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ train-web/src/main/java/com/train/web/trainweb/TrainWebApplication.java
@@ -0,0 +1,13 @@
+package com.train.web.trainweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainWebApplication.class, args);
+    }
+
+}
